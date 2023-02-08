f1f745781067f6ee149bcb6e8774bf17ca0fa459
FlorenceL453 <90851771+FlorenceL453@users.noreply.github.com>
2023-02-08 17:15:06 -0500

    lien js

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
-const btnCercle = document.querySelector('.button1');
-const son = new Audio('\media\Son\horse (1).mp3');
-
+const btnCercle = document.querySelector(".button1");
+const son = new Audio('/media/Son/horse (1).mp3');
+console.log(btnCercle);
 btnCercle.addEventListener('click', function(){
  son.play();
 });
