962b4a5156f4d7e7e4dbd3c5795a5de68ab8245a
FlorenceL453 <90851771+FlorenceL453@users.noreply.github.com>
2023-02-08 17:30:21 -0500

    javascript son

--- script/main.js
+++ script/main.js
@@ -1,6 +1,10 @@
-const btnCercle = document.querySelector(".button1");
+var btnCercle = document.getElementsByClassName("button");
+btnCercle = i= [0];
+//const btnCercle = document.querySelector(".button.button")
 const son = new Audio('/media/Son/horse (1).mp3');
+
 console.log(btnCercle);
+
 btnCercle.addEventListener('click', function(){
  son.play();
 });
