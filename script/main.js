3a9ad4f3cf7169f382235818564eaeaac2e319e0
FlorenceL453 <90851771+FlorenceL453@users.noreply.github.com>
2023-02-09 11:52:41 -0500

    ajout function html

--- script/main.js
+++ script/main.js
@@ -2,14 +2,18 @@
 //btnCercle = i= [0];
 //var btnCercle = document.getElementsByClassName('#button');
 
-const btnCercle = document.querySelector('.button');
+//const btnCercle = document.querySelector('.button');
 
 //btnCercle = i=[0];
 const son = new Audio('/media/Son/horse (1).mp3');
 
-console.log(btnCercle);
-
-btnCercle.addEventListener("click", function(){
+function joueSon(){
     son.play();
- onSendClick;
-});
+}
+
+//console.log(btnCercle);
+
+//btnCercle.addEventListener("click", function(){
+   // son.play();
+ //onSendClick;
+//});
